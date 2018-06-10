1f70e4fa0f8ccc7b48e6147b9640e04d174c1006
Dimannich <dim199616@mail.ru>
2018-06-10 14:09:13 +1000

    stable desktop

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+
+    function initMap() {
+        // Create a map object and specify the DOM element for display.
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 48.48001, lng: 135.07679},
+          zoom: 15
+        });
+    }
